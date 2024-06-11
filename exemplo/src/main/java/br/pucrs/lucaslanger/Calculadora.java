ab602dc6e392b263ea52e3ba9cb0fbad49a6cfc2
Lucas Lantmann <102996915+Lantmann@users.noreply.github.com>
2024-06-11 22:06:47 +0000

    Atualização dos testes

--- exemplo/src/main/java/br/pucrs/lucaslanger/Calculadora.java
+++ exemplo/src/main/java/br/pucrs/lucaslanger/Calculadora.java
@@ -7,17 +7,19 @@ import java.time.format.DateTimeParseException;
 public class Calculadora {
 
     public String validateDate(String dateStr) {
+        // Verificação do tamanho
         if (dateStr.length() != 10) {
             return "ERRO: tamanho invalido.";
         }
         
+        // Verificação do formato e barra "/"
         if (!dateStr.matches("\\d{2}/\\d{2}/\\d{4}")) {
             if (!dateStr.contains("/")) {
                 return "ERRO: nao utilizou '/'.";
             }
             return "ERRO: nao utilizou digito.";
         }
-        
+
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
         try {
             LocalDate date = LocalDate.parse(dateStr, formatter);
--- exemplo/src/test/java/br/pucrs/lucaslanger/CalculadoraTest.java
+++ exemplo/src/test/java/br/pucrs/lucaslanger/CalculadoraTest.java
@@ -10,40 +10,40 @@ public class CalculadoraTest {
     public void testNextDateRegularDay() {
         Calculadora calculadora = new Calculadora();
         LocalDate date = LocalDate.of(2024, 6, 6);
-        LocalDate expected = LocalDate.of(2024, 6, 7);
-        assertEquals(expected, calculadora.nextDate(date));
+        LocalDate dataEsperada = LocalDate.of(2024, 6, 7);
+        assertEquals(dataEsperada, calculadora.nextDate(date));
     }
 
     @Test
     public void testNextDateEndOfMonth() {
         Calculadora calculadora = new Calculadora();
         LocalDate date = LocalDate.of(2024, 1, 31);
-        LocalDate expected = LocalDate.of(2024, 2, 1);
-        assertEquals(expected, calculadora.nextDate(date));
+        LocalDate dataEsperada = LocalDate.of(2024, 2, 1);
+        assertEquals(dataEsperada, calculadora.nextDate(date));
     }
 
     @Test
     public void testNextDateEndOfYear() {
         Calculadora calculadora = new Calculadora();
         LocalDate date = LocalDate.of(2023, 12, 31);
-        LocalDate expected = LocalDate.of(2024, 1, 1);
-        assertEquals(expected, calculadora.nextDate(date));
+        LocalDate dataEsperada = LocalDate.of(2024, 1, 1);
+        assertEquals(dataEsperada, calculadora.nextDate(date));
     }
 
     @Test
     public void testNextDateLeapYear() {
         Calculadora calculadora = new Calculadora();
         LocalDate date = LocalDate.of(2024, 2, 28);
-        LocalDate expected = LocalDate.of(2024, 2, 29);
-        assertEquals(expected, calculadora.nextDate(date));
+        LocalDate dataEsperada = LocalDate.of(2024, 2, 29);
+        assertEquals(dataEsperada, calculadora.nextDate(date));
     }
 
     @Test
     public void testNextDateNonLeapYear() {
         Calculadora calculadora = new Calculadora();
         LocalDate date = LocalDate.of(2023, 2, 28);
-        LocalDate expected = LocalDate.of(2023, 3, 1);
-        assertEquals(expected, calculadora.nextDate(date));
+        LocalDate dataEsperada = LocalDate.of(2023, 3, 1);
+        assertEquals(dataEsperada, calculadora.nextDate(date));
     }
 
     @Test
@@ -51,6 +51,13 @@ public class CalculadoraTest {
         Calculadora calculadora = new Calculadora();
         String dateStr = "11/08/2024";
         assertNull(calculadora.validateDate(dateStr));
+    }
+
+    @Test
+    public void testInvalidInputLength() {
+        Calculadora calculadora = new Calculadora();
+        String dateStr = "2062024";
+        assertEquals("ERRO: tamanho invalido.", calculadora.validateDate(dateStr));
     }
 
     @Test
@@ -68,9 +75,16 @@ public class CalculadoraTest {
     }
 
     @Test
+    public void testValidateDateInvalidFormat() {
+        Calculadora calculadora = new Calculadora();
+        String dateStr = "23-02-2025";
+        assertEquals("ERRO: nao utilizou '/'.", calculadora.validateDate(dateStr));
+    }
+
+    @Test
     public void testValidateDateNoSlash() {
         Calculadora calculadora = new Calculadora();
-        String dateStr = "2300220022";
+        String dateStr = "0220122038";
         assertEquals("ERRO: nao utilizou '/'.", calculadora.validateDate(dateStr));
     }
 
@@ -78,6 +92,20 @@ public class CalculadoraTest {
     public void testValidateDateInvalid() {
         Calculadora calculadora = new Calculadora();
         String dateStr = "32/05/2019";
+        assertEquals("ERRO: data invalida.", calculadora.validateDate(dateStr));
+    }
+
+    @Test
+    public void testValidateDateInvalidBefore1600() {
+        Calculadora calculadora = new Calculadora();
+        String dateStr = "23/02/1500";
+        assertEquals("ERRO: data invalida.", calculadora.validateDate(dateStr));
+    }
+
+    @Test
+    public void testValidateDateInvalidAfter9998() {
+        Calculadora calculadora = new Calculadora();
+        String dateStr = "23/02/9999";
         assertEquals("ERRO: data invalida.", calculadora.validateDate(dateStr));
     }
 }
